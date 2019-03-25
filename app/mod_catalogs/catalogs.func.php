48ca94302d5390767303c10636b202ff50f6bbe9
Tatiana <batagaeva.ta@gmail.com>
2019-03-26 00:49:51 +0800

    Ошибка каталога

--- app/mod_catalogs/catalogs.func.php
+++ app/mod_catalogs/catalogs.func.php
@@ -6,24 +6,26 @@ class CatalogsController
 {
 	function show($url)
 	{
-		echo $url;
-		print (d()->Catalog->find_by_url($url));
-		// d()->this = d()->Catalog->find_by_url($url);
-		// if (d()->this->is_empty) {
-		// 	$mas = array ();
-		// 	$mas[] = array ('title' => 'Главная', 'link' => '/');
-		// 	$mas[] = array ('title' => 'Каталог', 'link' => 'catalogs');
-		// 	$mas[] = array ('title' => 'Ошибка 404');
-		// 	d()->message="Такой страницы не существует".d()->add(array('catalogs','url'=>$url));
-		// 	d()->breadcrumbs = $mas;
-		// 	return d()->error('404');
-		// }
-		// $mas = array ();
-		// $mas[] = array ('title' => 'Главная', 'link' => '/');
-		// $mas[] = array ('title' => 'Каталог', 'link' => 'catalogs');
-		// $mas[] = array ('title' => d()->this->title, 'link' => d()->this->link);
-		// d()->breadcrumbs = $mas;
+		// echo $url;
+		// print (d()->Catalog->find_by_url($url));
+		d()->this = d()->Catalog->find_by_url($url);
+		if (d()->this->is_empty) {
+			$mas = array ();
+			$mas[] = array ('title' => 'Главная', 'link' => '/');
+			$mas[] = array ('title' => 'Каталог', 'link' => 'catalogs');
+			$mas[] = array ('title' => 'Ошибка 404');
+			d()->message="Такой страницы не существует".d()->add(array('catalogs','url'=>$url));
+			d()->breadcrumbs = $mas;
+			return d()->error('404');
+		}
+		$mas = array ();
+		$mas[] = array ('title' => 'Главная', 'link' => '/');
+		$mas[] = array ('title' => 'Каталог', 'link' => 'catalogs');
+		$mas[] = array ('title' => d()->this->title, 'link' => d()->this->link);
+		d()->breadcrumbs = $mas;
 		
-		// print d()->view();
+		print d()->view();
 	}
 }
+
+?>
