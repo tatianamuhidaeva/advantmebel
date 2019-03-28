40ded2f0a67e748b95d6350d7ce98a13ff576dc4
Tatiana <batagaeva.ta@gmail.com>
2019-03-28 11:40:27 +0800

    Закрытие модалки по клику вне

--- images/js/popup.js
+++ images/js/popup.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  function openModal(){
+$(document).ready(function () {
+  function openModal() {
     if ($(window).width() < 768) {
       $('.popup').toggleClass('d-flex');
       $('.content').toggleClass('d-none');
@@ -8,27 +8,30 @@ $(document).ready(function(){
       $('body').css('overflow', 'hidden');
     }
   }
-  function closeModal(){
+
+  function closeModal() {
     if ($(window).width() < 768) {
       $('.popup').toggleClass('d-flex');
-        $('.content').toggleClass('d-none');
-  
-      } else {
-        $('.popup').toggleClass('d-flex');
-        $('body').css('overflow', '');
-      }
-      $('.popup-form').removeClass('d-none');
-      $('.popup-success').removeClass('d-flex');
+      $('.content').toggleClass('d-none');
+
+    } else {
+      $('.popup').toggleClass('d-flex');
+      $('body').css('overflow', '');
+    }
+    $('.popup-form').removeClass('d-none');
+    $('.popup-success').removeClass('d-flex');
   }
 
   $('.header-callback__btn').on('click', openModal);
   $('.popup__close').on('click', closeModal);
 
-  $(document).on('click', function(e){
-    if(!($(e.target).hasClass('popup-wrap')) 
-    && !($(e.target).parents('.popup-wrap').length)
-    && !($(e.target).hasClass('header-callback__btn')) ){
-      closeModal();
+  $(document).on('click', function (e) {
+    if ($(window).width() >= 768) {
+      if (!($(e.target).hasClass('popup-wrap')) &&
+        !($(e.target).parents('.popup-wrap').length) &&
+        !($(e.target).hasClass('header-callback__btn'))) {
+        closeModal();
+      }
     }
   });
 });
